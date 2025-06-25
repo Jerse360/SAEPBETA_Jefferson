bba4f592691a77cba13f98f9c881fea27f80cf3a
GermanRz <germanramirez@outlook.com>
2025-06-25 11:48:26 -0500

    bug lamado a modelos sedes en plantilla.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once "controladores/sedes.controlador.php";
 require_once "modelos/fichas.modelo.php";
 require_once "modelos/usuarios.modelo.php";
 require_once "modelos/programas.modelo.php";
-require_once "controladores/sedes.controlador.php";
+require_once "modelos/sedes.modelo.php";
 
 $plantilla = new ControladorPlantilla();
 $plantilla -> ctrPlantilla();
